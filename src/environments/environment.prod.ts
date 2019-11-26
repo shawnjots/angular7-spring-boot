6fcff827ce6e26c51ae4f58f2d5b03836ad0a3f4
Shawn Majoto <shawnjots@gmail.com>
2019-11-26 15:38:09 +0200

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+const BASE_URI = '35.202.23.45';
+
+export const environment = {
+  production: true,
+  ENV: BASE_URI
+};
+
+
---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,8 @@
+const BASE_URI = '35.202.23.46';
+
+export const environment = {
+  production: true,
+  ENV: BASE_URI
+};
+
+
